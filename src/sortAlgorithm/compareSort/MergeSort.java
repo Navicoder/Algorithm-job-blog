b5b9b63b537b99312aeeeccb8cfc0bf125b1d760
wangwenchao <wangwenchao3@jd.com>
2018-09-06 17:01:59 +0800

    算法

---
+++ src/sortAlgorithm/compareSort/MergeSort.java
@@ -0,0 +1,62 @@
+package sortAlgorithm.compareSort;
+
+import utils.ArrUtils;
+
+/**
+ * 合并排序 分为递归实现与非递归(迭代)实现
+ */
+public class MergeSort {
+
+    // 递归实现的归并排序(自顶向下)
+    public static void MergeSortRecursion(int arr[], int left, int right){
+        if (left == right)    // 当待排序的序列长度为1时，递归开始回溯，进行merge操作
+            return;
+        int mid = (left + right) / 2;
+        MergeSortRecursion(arr, left, mid);
+        MergeSortRecursion(arr, mid + 1, right);
+        Merge(arr, left, mid, right);
+    }
+
+    // 非递归(迭代)实现的归并排序(自底向上)
+    public static void MergeSortIteration(int arr[], int len){
+        int left, mid, right;// 子数组索引,前一个为arr[left...mid]，后一个子数组为arr[mid+1...right]
+        // 子数组的大小i初始为1，每轮翻倍
+        for (int i = 1; i < len; i *= 2){
+            left = 0;
+            // 后一个子数组存在(需要归并)
+            while (left + i < len){
+                mid = left + i - 1;
+                right = mid + i < len ? mid + i : len - 1;// 后一个子数组大小可能不够
+                Merge(arr, left, mid, right);
+                left = right + 1;               // 前一个子数组索引向后移动
+            }
+        }
+    }
+    // 合并两个已排好序的数组arr[left...mid]和arr[mid+1...right]
+    public static void Merge(int arr[], int left, int mid, int right){
+        int len = right - left + 1;
+        int[] brr = new int[len];       // 辅助空间O(n)
+        int index = 0;
+        int i = left;                   // 前一数组的起始元素
+        int j = mid + 1;                // 后一数组的起始元素
+        while (i <= mid && j <= right){
+            brr[index++] = arr[i] <= arr[j] ? arr[i++] : arr[j++];  // 带等号保证归并排序的稳定性
+        }
+        while (i <= mid){
+            brr[index++] = arr[i++];
+        }
+        while (j <= right){
+            brr[index++] = arr[j++];
+        }
+        for (int k = 0; k < len; k++){
+            arr[left++] = brr[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = ArrUtils.generalRandomOrder(11,5000);
+        int[] arr2= ArrUtils.copyArr(arr);
+        MergeSort.MergeSortIteration(arr2,arr2.length);
+        ArrUtils.printArr(arr2);
+    }
+}
--- src/sortAlgorithm/compareSort/Test1.java
+++ src/sortAlgorithm/compareSort/Test1.java
@@ -1,14 +1,13 @@
 package sortAlgorithm.compareSort;
 
-import jdk.nashorn.tools.Shell;
 import utils.ArrUtils;
 
 import java.util.Arrays;
 
 public class Test1 {
     public static void main(String[] args) {
-        int[] arr = ArrUtils.generalRandomOrder(1000000,1000000000);
-        //int[] arr = ArrUtils.generalNearlyOrder(1000000,1000000000);
+        //int[] arr = ArrUtils.generalRandomOrder(1000000,1000000000);
+        int[] arr = ArrUtils.generalNearlyOrder(1000000,1000000000);
         int[] arr0=ArrUtils.copyArr(arr);
 
 
@@ -45,50 +44,43 @@ public class Test1 {
     }
 
     /**
-     *  【1】ArrUtils.generalRandomOrder(1000000,89602);100万 在0~9万之间 有很多重复的
-     * true
-     * true
-     * 冒泡排序:2131509ms 2131秒 35分钟
-     * 选择排序:397783ms  397秒  6分钟
-     * 插入排序:256444ms  256秒  4分钟
-     * true
-     * true
      *
      * ArrUtils.generalRandomOrder(1000000,1000000000); 100万 在0~10亿 重复的很少
      *
      * true
      * true
      * true
-     * 希尔排序:274ms,0秒0分钟0小时
-     * 插入排序:206259ms,206秒3分钟0小时
-     * 选择排序:376675ms,376秒6分钟0小时
-     * 冒泡排序:2018527ms,2018秒33分钟0小时
      * true
+     * Arrays.sort:195ms
+     * 希尔排序shellSort:280ms,0秒0分钟0小时
+     * 希尔排序shellSortDichotomy:350ms,0秒0分钟0小时
+     * 插入排序:220921ms,220秒3分钟0小时
+     * 选择排序:402014ms,402秒6分钟0小时
+     * 冒泡排序:2044056ms,2044秒34分钟0小时
      * true
-     * true
-     *
-     * ArrUtils.generalRandomOrder(10000,1000000000);1 万 在0~10亿 几乎没有重复的
-     *
-     * true
-     * true
-     * true
-     * 希尔排序:5ms,0秒0分钟0小时
-     * 插入排序:37ms,0秒0分钟0小时
-     * 选择排序:54ms,0秒0分钟0小时
-     * 冒泡排序:208ms,0秒0分钟0小时
      * true
      * true
      * true
      *
+
      *
      * ArrUtils.generalNearlyOrder(1000000,1000000000);100万 在0~10亿 几乎有序
      * true
      * true
-     * 插入排序:6ms,0秒0分钟0小时
-     * 选择排序:159296ms,159秒2分钟0小时
-     * 冒泡排序:183095ms,183秒3分钟0小时
      * true
      * true
+     * Arrays.sort:8ms
+     * 希尔排序shellSort:29ms,0秒0分钟0小时
+     * 希尔排序shellSortDichotomy:41ms,0秒0分钟0小时
+     * 插入排序:8ms,0秒0分钟0小时
+     * 选择排序:167274ms,167秒2分钟0小时
+     * 冒泡排序:176085ms,176秒2分钟0小时
+     * true
+     * true
+     * true
+     * true
+     * true
+     *
      *
      */
 }
